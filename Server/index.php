dd8b4ac3cd58bea6527bf40ea4c5af6b32209fb5
unknown <jacobkorede5@gmail.com>
2024-03-27 03:49:33 +0100

    working

--- Server/index.php
+++ Server/index.php
@@ -5,45 +5,93 @@ if ($db->connect_errno) {
     die('sorry we are experiencing some errors');
 }
 
-if (isset($_POST['upload'])) {
-    $filename = $_FILES["uploadfile"]["name"];
-    $tempname = $_FILES["uploadfile"]["tmp_name"];
-    $folder = "./image/" . $filename;
+$routes = [
+    '/' => 'home',
+    '/upload' => 'uploadImage',
+    '/addComment' => 'addComment',
+    '/getComments' => 'getComments'
+];
 
-    $sql = "INSERT INTO image (filename) VALUES ('$filename')";
+function home()
+{
+    echo 'home page';
+}
 
-    mysqli_query($db, $sql);
+function uploadImage()
+{
+    global $db;
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    if (move_uploaded_file($tempname, $folder)) {
-        echo '<h3> image uploaded successfully! </h3';
-    } else {
-        echo 'image failed to load';
+        if (isset($_FILES['file'])) {
+            $filename = $_FILES["file"]["name"];
+            $tempname = $_FILES["file"]["tmp_name"];
+            $folder = "./images/" . $filename;
+            $title = isset($_POST['title']) ? $_POST['title'] : '';
+            $sql = "INSERT INTO image (filename , title) VALUES ('$filename','$title')";
+
+            if (mysqli_query($db, $sql)) {
+                http_response_code(201);
+                echo "movie uploaded successfully";
+            } else {
+                echo "movie failed to upload";
+            }
+
+            if (move_uploaded_file($tempname, $folder)) {
+                echo 'image uploaded successfully! ';
+            } else {
+                echo 'image failed to load';
+            }
+        }
     }
 }
 
-function addComment($movieid, $commentText)
+function addComment()
 {
     global $db;
-    $sql = "INSERT INTO comments (movie,comment) VALUES ('$movieid','$commentText')";
-    if ($db->query($sql) === TRUE) {
-        return true;
-    } else {
-        return false;
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $post_data = file_get_contents('php://input');
+        $params = json_decode($post_data, true);
+        $movieid = '';
+        $commentText = '';
+        if (isset($params['movie'])) {
+            $movieid = $params['movie'];
+        }
+        if (isset($params['comment'])) {
+            $commentText = $params['comment'];
+        }
+        $sql = "INSERT INTO comments (movie,comment) VALUES ('$movieid','$commentText')";
+        if (mysqli_query($db, $sql)) {
+            http_response_code(201);
+            echo 'comment added successfully';
+        } else {
+            echo 'failed to add comment';
+        }
     }
 }
 
-function getComments($movieid)
+function getComments()
 {
     global $db;
-    $sql = "SELECT * FROM comments WHERE movie = '$movieid' ";
+    $sql = "SELECT * FROM comments";
     $result = $db->query($sql);
     $comments = array();
     if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $comments[] = $row['comment'];
-        }
+        $data = $result->fetch_all(MYSQLI_ASSOC);
+        header('Content-Type:application/json');
+        echo json_encode($data);
+    } else {
+        echo "no data found";
     }
-    return $comments;
 }
+
+$request_uri = $_SERVER['REQUEST_URI'];
+if (!array_key_exists($request_uri, $routes)) {
+    http_response_code((404));
+    echo "404 Not Found";
+    exit;
+}
+
+$route_function = $routes[$request_uri];
+$route_function();
 
 $db->close();
